cda4f30eb62fcab137ac87fed545bf46a099b9dd
Vladimir Osipov <vladimir.osipov@coursio.com>
2019-11-05 21:46:47 +0200

    experimental remote terminal plugin

---
+++ nisdos/terminal/src/index.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import UI from './ui'
+
+XROOM_PLUGIN({
+
+  translations: {
+    en: {
+      iconCaption: 'Terminal',
+      btnClose: 'Close',
+    },
+    sv: {
+      iconCaption: 'Terminal',
+      btnClose: 'Close',
+    },
+    ru: {
+      iconCaption: 'Terminal',
+      btnClose: 'Close',
+    },
+  },
+
+  register () {
+    this.onRoomEnter = this.onRoomEnter.bind(this)
+    this.onRoomExit = this.onRoomExit.bind(this)
+    this.onDataIn = this.onDataIn.bind(this)
+
+    window.addEventListener('room/enter', this.onRoomEnter)
+    window.addEventListener('room/exit', this.onRoomExit)
+    window.addEventListener('data/in', this.onDataIn)
+
+    this.api('appendScript', {src: '/plugins/nisdos/terminal/xterm.js'})
+    this.api('appendStyle', {src: '/plugins/nisdos/terminal/xterm.css'})
+
+    this.api('addUI', { component:
+      <UI
+        i18n={this.i18n}
+        api={this.api}
+        ref={(ref) => { this.ui = ref} }
+      />
+    })
+  },
+
+  unregister () {
+    window.removeEventListener('room/enter', this.onRoomEnter)
+    window.removeEventListener('room/exit', this.onRoomExit)
+    window.removeEventListener('data/in', this.onDataIn)
+    this.api('removeIcon')
+  },
+
+  addIcon () {
+    this.api('addIcon', {
+      title: this.i18n.t('iconCaption'),
+      onClick: () => this.ui.open(),
+      svg: props =>
+        <svg xmlns="http://www.w3.org/2000/svg" width={props.size || 24} height={props.size || 24} viewBox="0 0 24 24">
+          <path fill={props.color || '#000'} d='M20,19V7H4V19H20M20,3A2,2 0 0,1 22,5V19A2,2 0 0,1 20,21H4A2,2 0 0,1 2,19V5C2,3.89 2.9,3 4,3H20M13,17V15H18V17H13M9.58,13L5.57,9H8.4L11.7,12.3C12.09,12.69 12.09,13.33 11.7,13.72L8.42,17H5.59L9.58,13Z' />
+        </svg>
+    })
+  },
+
+  isSupported () {
+    return true
+  },
+
+  onRoomEnter () {
+    this.addIcon()
+    this.api('renderControls')
+  },
+
+  onRoomExit () {
+    this.api('removeIcon')
+  },
+
+  onDataIn (event) {
+    const { pluginId, cmd, args } = event.detail
+
+    if (pluginId !== this.id || !cmd) return
+    this.ui.dataIn({cmd, args})
+  }
+})
---
+++ nisdos/terminal/src/ui.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+
+class UI extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.term = null
+    this.state = {
+      isShown: false,
+    }
+
+    this.open = this.open.bind(this)
+    this.close = this.close.bind(this)
+  }
+
+  componentDidUpdate() {
+    if (this.state.isShown && window.Terminal) {
+      this.term = new window.Terminal()
+      this.term.open(document.getElementById('plugin-nisdos-terminal'))
+      this.term.write('Terminal v1.0, awaiting connection...')
+
+      this.props.api('broadcastData', {cmd: 'init', args: []})
+      this.term._core._onKey._listeners.push(({key, domEvent}) => {
+
+        switch (domEvent.keyCode) {
+          case 8:
+            this.term.write('\x1b[D')
+            this.props.api('broadcastData', {cmd: 'key', args: ['\b \b']})
+            break
+
+          case 13:
+            this.term.write('\n\r')
+            this.props.api('broadcastData', {cmd: 'line', args: [' ']})
+            break
+
+          default:
+            this.term.write(key)
+            this.props.api('broadcastData', {cmd: 'key', args: [key]})
+        }
+      })
+    }
+  }
+
+  open () {
+    this.setState({isShown: true})
+    this.props.api('setHotKeysEnable', false)
+  }
+
+  close () {
+    this.setState({isShown: false})
+    this.props.api('setHotKeysEnable', true)
+  }
+
+  dataIn ({cmd, args}) {
+    if (cmd === 'line') {
+      for (const line of args[0].split('\n')) {
+        this.term.writeln(line)
+      }
+      this.term.write('> ')
+    }
+    if (cmd === 'init-ok') {
+      this.term.write('\x1bc> ')
+    }
+  }
+
+  render () {
+    const { i18n } = this.props
+    const { isShown } = this.state
+
+    if (!isShown) {
+      return null
+    }
+
+    return (
+      <div style={styles.ui}>
+        <div style={styles.box}>
+          <div id="plugin-nisdos-terminal" />
+          <button
+            onClick={this.close}
+            style={styles.button}
+          >
+            { i18n.t('btnClose') }
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const styles = {
+  ui: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    background: 'transparent',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    maxWidth: '100vw',
+    background: '#333',
+    border: '2px solid #fff'
+  },
+  button: {
+    margin: '8px',
+  },
+}
+
+export default UI
