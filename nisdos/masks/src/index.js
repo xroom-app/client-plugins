03267c08a2f82825cb86031d9470e507b23df911
Vladimir Osipov <vladimir.osipov@coursio.com>
2020-05-11 00:13:34 +0300

    fix black screen when plugin is started directly after install

--- nisdos/masks/src/index.js
+++ nisdos/masks/src/index.js
@@ -13,19 +13,7 @@ function onStreamChanged (data) {
   if (data.videoOn) {
     this.camLoaded = true
     this.videoStream = new MediaStream(data.stream.getVideoTracks())
-
-    const settings = this.videoStream.getVideoTracks()[0].getSettings()
-
-    if (settings && settings.width) {
-      this.aspectRatio = settings.height / settings.width
-      this.mats.src = new cv.Mat(320 * this.aspectRatio, 320, cv.CV_8UC4)
-      this.mats.dst = new cv.Mat(320 * this.aspectRatio, 320, cv.CV_8UC4)
-      console.log('Aspect ratio', this.aspectRatio)
-    } else {
-      this.mbox({text: 'This browser does not support fully support masks. Video may be squeezed.'})
-      this.mats.src = new cv.Mat(240, 320, cv.CV_8UC4)
-      this.mats.dst = new cv.Mat(240, 320, cv.CV_8UC4)
-    }
+    this.createMats()
 
     if (this.cvLoaded) {
       if (!this.cvVideoBuffer) {
@@ -106,6 +94,14 @@ XROOM_PLUGIN({
                   this.camLoaded = true
                 }
               }
+
+              if (!this.mats.dst) {
+                this.createMats()
+              }
+
+              if (!this.cvVideoBuffer) {
+                this.prepare()
+              }
             } else {
               console.log('Failed to load datafile. Status: ' + request.status)
             }
@@ -136,6 +132,22 @@ XROOM_PLUGIN({
           <path fill={props.color || '#000'} d="M21,13A9,9 0 0,1 12,22A9,9 0 0,1 3,13L3.03,4.43C5.68,2.88 8.76,2 12.05,2C15.3,2 18.36,2.87 21,4.38V13M13,19.93C16.39,19.44 19,16.5 19,13V5.59C16.9,4.57 14.54,4 12.05,4C9.5,4 7.08,4.6 4.94,5.66L5,13C5,16.5 7.63,19.44 11,19.93V18H13V19.93M11,16H8L6,13L9,14H10L11,13H13L14,14H15L18,13L16,16H13L12,15L11,16M6,9.03C6.64,8.4 7.5,8.05 8.5,8.05C9.45,8.05 10.34,8.4 11,9.03C10.34,9.65 9.45,10 8.5,10C7.5,10 6.64,9.65 6,9.03M13,9.03C13.64,8.4 14.5,8.05 15.5,8.05C16.45,8.05 17.34,8.4 18,9.03C17.34,9.65 16.45,10 15.5,10C14.5,10 13.64,9.65 13,9.03Z" />
         </svg>
     })
+  },
+
+
+  createMats() {
+    const settings = this.videoStream.getVideoTracks()[0].getSettings()
+
+    if (settings && settings.width) {
+      this.aspectRatio = settings.height / settings.width
+      this.mats.src = new cv.Mat(320 * this.aspectRatio, 320, cv.CV_8UC4)
+      this.mats.dst = new cv.Mat(320 * this.aspectRatio, 320, cv.CV_8UC4)
+      console.log('Aspect ratio', this.aspectRatio)
+    } else {
+      this.mbox({text: 'This browser does not support fully support masks. Video may be squeezed.'})
+      this.mats.src = new cv.Mat(240, 320, cv.CV_8UC4)
+      this.mats.dst = new cv.Mat(240, 320, cv.CV_8UC4)
+    }
   },
 
   async loadMasks () {
