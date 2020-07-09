6abb730e67b95aab8e903e9003e61d159ab7ada9
Nikolay Leonov <leonov.nikolay@euroauto.ru>
2020-07-09 23:36:26 +0300

    Save image, highlight current tool

--- nisdos/sketcher/src/ui.js
+++ nisdos/sketcher/src/ui.js
@@ -32,6 +32,11 @@ class UI extends Component {
     }
   }
 
+  saveImage = () => {
+    var image = this.draw.canvas.drawing.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    window.location.href = image;
+  }
+
   render () {
     const { isShown, color, tool } = this.state
 
@@ -50,6 +55,7 @@ class UI extends Component {
     }
 
     const colors = ['black', 'blue', 'green', 'red', 'yellow']
+
 
     return (
       <div style={styles.ui}>
@@ -73,19 +79,19 @@ class UI extends Component {
             })
           }
 
-          <div onClick={() => this.setState({tool: 0})} style={styles.button}>
+          <div onClick={() => this.setState({tool: 0})} style={{...styles.button, backgroundColor: this.state.tool === 0 ? "#B0B0B0" : "#FFFFFF"}}>
             <svg style={{width: '32px', height: '32px'}} viewBox="0 0 24 24">
               <path fill="333" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
             </svg>
           </div>
 
-          <div onClick={() => this.setState({tool: 1})} style={styles.button}>
+          <div onClick={() => this.setState({tool: 1})} style={{...styles.button, backgroundColor: this.state.tool === 1 ? "#B0B0B0" : "#FFFFFF"}}>
             <svg style={{width: '32px', height: '32px'}} viewBox="0 0 24 24">
               <path fill="#333" d="M19,6H22V8H19V11H17V8H14V6H17V3H19V6M17,17V14H19V19H3V6H11V8H5V17H17Z" />
             </svg>
           </div>
 
-          <div onClick={() => this.setState({tool: 2})} style={styles.button}>
+          <div onClick={() => this.setState({tool: 2})} style={{...styles.button, backgroundColor: this.state.tool === 2 ? "#B0B0B0" : "#FFFFFF"}}>
             <svg style={{width: '32px', height: '32px'}} viewBox="0 0 24 24">
               <path fill="none" stroke="#333" strokeWidth="1.97675" d="M 17.97679,13.853333 A 7.182672,6.8464398 0 0 1 13.542807,20.178619 7.182672,6.8464398 0 0 1 5.7152018,18.694497 7.182672,6.8464398 0 0 1 4.1581943,11.233314 7.182672,6.8464398 0 0 1 10.794118,7.0068936" />
               <rect fill="#333" y="3" x="17" height="8" width="2" />
@@ -93,7 +99,7 @@ class UI extends Component {
             </svg>
           </div>
 
-          <div onClick={() => this.setState({tool: 3})} style={styles.button}>
+          <div onClick={() => this.setState({tool: 3})} style={{...styles.button, backgroundColor: this.state.tool === 3 ? "#B0B0B0" : "#FFFFFF"}}>
             <svg
               style={{width: '32px', height: '32px'}}
               viewBox="0 0 24 24" >
@@ -107,10 +113,14 @@ class UI extends Component {
             </svg>
           </div>
 
-          <div onClick={() => this.setState({tool: 4})} style={styles.button}>
+          <div onClick={() => this.setState({tool: 4})} style={{...styles.button, backgroundColor: this.state.tool === 4 ? "#B0B0B0" : "#FFFFFF"}}>
             <svg style={{width: '32px', height: '32px'}} viewBox="0 0 24 24">
               <path fill="#333" d="M9.6,14L12,7.7L14.4,14M11,5L5.5,19H7.7L8.8,16H15L16.1,19H18.3L13,5H11Z" />
             </svg>
+          </div>
+
+          <div onClick={this.saveImage} style={styles.save}>
+            Save
           </div>
 
         </div>
@@ -139,19 +149,27 @@ const styles = {
     backgroundColor: '#fff',
   },
   controls: {
-    paddingTop: '3px',
+    padding: '3px 0px',
     borderBottom: '1px solid #aaa',
     display: 'flex',
   },
   button: {
     cursor: 'pointer',
     margin: '0 2px',
+    height: '32px',
+    borderRadius: '4px',
   },
   canvas: {
     width: '100%',
     height: '100%',
     cursor: 'crosshair',
   },
+  save: {
+    cursor: "pointer",
+    margin: "0px 10px 0px auto",
+    fontWeight: "bold",
+    fontSize: "24px",
+  }
 }
 
 if (window.matchMedia('screen and (max-width: 480px)').matches) {
