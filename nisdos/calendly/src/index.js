48fd888b1916ab2751c1e375927db5d7f74ef263
Vladimir Osipov <vladimir.osipov@coursio.com>
2020-08-09 00:36:19 +0300

    Calendly plugin

--- nisdos/calendly/src/index.js
+++ nisdos/calendly/src/index.js
@@ -2,10 +2,21 @@ import React from 'react'
 import UI from './ui'
 
 XROOM_PLUGIN({
-
   calendlyScriptRef: null,
 
   register () {
+
+    this.api('addUI', {
+      component: <UI
+        api={this.api}
+        mbox={this.mbox}
+        ref={(ref) => { this.ui = ref} }
+      />
+    })
+
+    this.api('appendScript', {src: 'https://assets.calendly.com/assets/external/widget.js'}).then((id) => {
+      this.calendlyScriptRef = id
+    })
 
     this.api('addIcon', {
       title: 'Calendly',
@@ -14,14 +25,6 @@ XROOM_PLUGIN({
         <svg xmlns="http://www.w3.org/2000/svg" width={props.size || 24} height={props.size || 24} viewBox="0 0 100 100">
           <path fill={props.color || '#000'} d="M6.6 99.3c-2.4-.8-4.5-2.7-5.7-5-.9-1.9-.9-4-.9-42 0-44.8-.2-42 3.5-45.2C6.1 5 8 4.6 16.7 4.5h8V2.7c.3-2 1-2.8 2.4-2.8 1.6 0 2.6 1.2 2.6 3v1.7h40.5l.1-2c.2-1.7.4-2 1.6-2.4 1.9-.7 3 0 3.2 2.3l.3 1.8 8 .1c8.7.1 10.5.5 13 2.6 3.8 3.2 3.6.4 3.6 45.2 0 37.7 0 40.2-.9 42a11 11 0 0 1-6.3 5.2c-1.2.4-15.7.5-43 .5-33.8 0-41.6-.1-43.2-.7zm86.6-5.8l1.5-1.3V12.5L93.2 11l-1.4-1.3H75.1v4.4c0 4.3 0 4.4-1.2 5-1 .4-1.4.3-2.4-.3-1.2-.7-1.2-1-1.2-5V9.8H29.7v4.1c0 4 0 4.3-1.2 5-1 .6-1.5.7-2.4.3-1.2-.6-1.2-.7-1.2-5V9.8h-8.3c-9.3 0-10.3.3-11.2 3.6-.7 2.4-.7 75.5 0 77.9A6 6 0 0 0 7 94c1.3.9 1.7.9 43 .9h41.7zM48.5 69.7a18.1 18.1 0 0 1-11.8-9.6c-1-2-1.2-3.3-1.2-6 0-5 1.3-8.2 4.8-11.6 4.5-4.4 10-6 16.4-4.8 3.4.6 7.7 2.6 7.7 3.5 0 1-.9.8-2.5-.3-3.6-2.4-10.1-3-14.8-1.3a18.7 18.7 0 0 0-9 8c-1 2-1.2 3.2-1.2 6.1 0 4.4 1 7 3.9 10 2.5 2.7 5 4.1 8.6 5 4.7 1.1 7 .7 14.6-3 .2-.1.4.1.4.5s-1 1.3-2 1.9c-4.2 2.3-9.6 3-14 1.6z" />
         </svg>
-    })
-
-    this.api('appendScript', {src: 'https://assets.calendly.com/assets/external/widget.js'}).then((id) => {
-      this.calendlyScriptRef = id
-    })
-
-    this.api('addUI', {
-      component: <UI ref={(ref) => { this.ui = ref} }/>
     })
   },
 
--- nisdos/calendly/src/ui.js
+++ nisdos/calendly/src/ui.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 // styling docs: https://help.calendly.com/hc/en-us/articles/360019861794-Common-embed-questions#1
+
+const backgroundImage = "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle'%3ELoading...%3C/text%3E%3C/svg%3E\") no-repeat center"
 
 class UI extends Component {
 
@@ -8,52 +10,98 @@ class UI extends Component {
     super(props)
 
     this.state = {
+      saved: false,
       isShown: false,
-      extId: false,
+      extId: (JSON.parse(localStorage.getItem('nisdos/calendly')) ?? {}).extId || null,
     }
-
-    this.toggleShow = this.toggleShow.bind(this)
-    this.onExtIdChange = this.onExtIdChange.bind(this)
   }
 
   toggleShow () {
     const { isShown } = this.state
+    this.setState({isShown: !isShown})
+  }
 
+  initWidget (username) {
     if (!window.Calendly) {
       alert('We couldn\'t load Calendly widget from their site. That\'s strange.')
       return
     }
 
-    if (!isShown) {
-
-      setTimeout(() => {
-        window.Calendly.initInlineWidget({
-          url: 'https://calendly.com/vladimir-osipov/?hide_event_type_details=1&hide_landing_page_details=1',
-          parentElement: document.getElementById('calendly-container'),
-        })
-      }, 50)
-    }
-
-    this.setState({isShown: !isShown})
+    setTimeout(() => {
+      window.Calendly.initInlineWidget({
+        url: `https://calendly.com/${username}/?hide_event_type_details=1&hide_landing_page_details=1`,
+        parentElement: document.getElementById('calendly-container'),
+      })
+    }, 50)
   }
 
-  onExtIdChange (ev) {
+  onExtIdChange = (ev) => {
+    this.setState({extId: ev.target.value.trim()})
+  }
 
+  onExtIdKeyDown = (ev) => {
+    ev.stopPropagation()
+
+    if (ev.key === 'Enter') {
+      setTimeout(this.onSave, 100)
+    }
+  }
+
+  onSave = () => {
+    const { extId } = this.state
+
+    localStorage.setItem('nisdos/calendly', JSON.stringify({extId}))
+    this.setState({saved: true})
+    this.initWidget(extId)
+  }
+
+  onReset = () => {
+    this.setState({saved: false})
+  }
+
+  onShare = () => {
+    this.props.api('sendMessage', {to: null, message: `Book me here: https://calendly.com/${this.state.extId}`})
+    this.props.mbox({text: 'Sent to everyone in the chat'})
   }
 
   render () {
-
-    const { isShown } = this.state
-
-    if (!isShown) {
-      return null
-    }
+    const { isShown, saved, extId } = this.state
 
     return (
-      <div style={styles.ui} onClick={() => this.setState({isShown: false})}>
-        <div style={styles.box}>
-          <input type="text" onChange={this.onExtIdChange} placeholder="Your Calendly link" style={styles.extIdInput}/>
-          <div id="calendly-container" style={{flex: 1}}/>
+      <div style={{...styles.ui, ...(isShown ? {} : styles.uiOffset)}}>
+        <div style={saved ? styles.fullBox : styles.box}>
+          {
+            saved ?
+              <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
+                <div id="calendly-container" style={{flex: 1, background: backgroundImage}} />
+                <div>
+                  <button style={styles.topButton} onClick={this.onReset}>Reset</button>
+                  <button style={styles.topButton} onClick={this.onShare}>Share</button>
+                </div>
+              </div>
+              :
+              <Fragment>
+                <div style={styles.url}>
+                  <span>https://calendly.com/</span>
+                  <input
+                    defaultValue={extId}
+                    type="text"
+                    onChange={this.onExtIdChange}
+                    placeholder="username"
+                    style={styles.extIdInput}
+                    autoFocus={true}
+                    onKeyDown={this.onExtIdKeyDown}
+                    onKeyUp={ev => ev.stopPropagation()}
+                  />
+                </div>
+                <button
+                  style={styles.extIdSave}
+                  onClick={this.onSave}
+                >
+                  Confirm
+                </button>
+              </Fragment>
+          }
         </div>
       </div>
     )
@@ -72,15 +120,47 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
   },
+  uiOffset: {
+    position: 'fixed',
+    top: '100vh',
+    left: '100vw',
+  },
   box: {
+    background: '#fff',
+    width: '360px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: '16px',
+    borderRadius: '4px',
+  },
+  fullBox: {
+    display: 'flex',
+    borderRadius: '4px',
     background: '#fff',
     minWidth: '640px',
     minHeight: '500px',
-    display: 'flex',
+    overflow: 'hidden',
     flexDirection: 'column',
   },
+  url: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: '15px',
+  },
   extIdInput: {
-    lineHeight: '',
+    border: '0.5px solid #888',
+    borderRadius: '4px',
+    padding: '2px',
+    fontSize: '15px',
+    width: '202px',
+  },
+  extIdSave: {
+    marginTop: '16px',
+    cursor: 'pointer',
+  },
+  topButton: {
+    margin: '8px',
   },
 }
 
