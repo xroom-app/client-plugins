300d4b76b8a0703650d24378aa31d515f2fcca59
Vladimir Osipov <vladimir.osipov@coursio.com>
2020-09-15 01:25:45 +0300

    graphics changed

--- nisdos/vocoder/src/index.js
+++ nisdos/vocoder/src/index.js
@@ -104,8 +104,8 @@ XROOM_PLUGIN({
       title: () => this.i18n.t('iconCaption'),
       onClick: () => this.ui.toggle(this.ctx, this.source),
       svg: () => (
-        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 50 50">
-          <path fill="#DDD" d="M7.04 10.21H43c1.06 0 1.89.81 1.89 1.84v25.8c0 1.04-.83 1.85-1.9 1.85H7.05c-1.06 0-1.9-.81-1.9-1.84v-25.8c0-1.04.84-1.85 1.9-1.85zm0-1.84a3.75 3.75 0 0 0-3.79 3.68v25.8c0 2.03 1.7 3.7 3.79 3.7H43a3.75 3.75 0 0 0 3.78-3.7v-25.8c0-2.02-1.7-3.68-3.78-3.68zM6 9h38c1.12 0 2 .88 2 2v28c0 1.12-.88 2-2 2H6c-1.12 0-2-.88-2-2V11c0-1.12.88-2 2-2zm0-2c-2.2 0-4 1.8-4 4v28c0 2.2 1.8 4 4 4h38c2.2 0 4-1.8 4-4V11c0-2.2-1.8-4-4-4z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+          <path fill="#DDD" d="M3,17V19H9V17H3M3,5V7H13V5H3M13,21V19H21V17H13V15H11V21H13M7,9V11H3V13H7V15H9V9H7M21,13V11H11V13H21M15,9H17V7H21V5H17V3H15V9Z"/>
         </svg>)
     })
   },
