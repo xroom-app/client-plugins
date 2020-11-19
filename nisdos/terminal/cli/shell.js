1ed62e81c7ac1960b0734a63f18aff72f0335d1c
Vladimir Osipov <vladimir.osipov@coursio.com>
2020-11-19 13:09:54 +0200

    restart the shell on exit

--- nisdos/terminal/cli/shell.js
+++ nisdos/terminal/cli/shell.js
@@ -9,7 +9,6 @@ class Shell {
 
   init({command = null, sendData}) {
     this.sh = spawn(this.command)
-
     this.sh.stdin.resume()
 
     this.sh.stdout.on('data', function (data) {
@@ -20,8 +19,9 @@ class Shell {
       sendData(data.toString())
     })
 
-    this.sh.on('close', function () {
-      sendData('Bye-bye!')
+    this.sh.on('close', () => {
+      sendData('You killed the shell, so we restarted it. :)')
+      this.init({sendData})
     })
   }
 
