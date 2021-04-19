a544a532d840d0ec86b287a5421c91ad4cc4df4b
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-04-19 17:37:00 +0300

    copies for the revamped version, icon anim fix

--- nisdos/screen-recorder/src/icon.js
+++ nisdos/screen-recorder/src/icon.js
@@ -52,7 +52,7 @@ export default class IconSvg extends React.Component {
       <svg width={size || 25} height={size || 25} fill="none" viewBox="0 0 27 25" xmlns="http://www.w3.org/2000/svg">
         <path d="M4.7 18.8h15.6c.9 0 1.6-.7 1.6-1.6v-11c0-.8-.7-1.5-1.6-1.5H4.7c-.9 0-1.6.7-1.6 1.5v11c0 .9.7 1.6 1.6 1.6zM15.6 21.9H9.4" stroke={ color } stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M15.6 11.7L11 8.6v6.2l4.7-3z" stroke={ color } stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round"/>
-        <circle cx="21" cy="20" r="5" fill={ blink ? '#e04006' : color } stroke={ blink ? '#e04006' : color } />
+        <circle cx="21" cy="20" r="5" fill={ blink ? '#e04006' : 'transparent' } stroke={ blink ? '#e04006' : 'transparent' } />
       </svg>
     )
   }
