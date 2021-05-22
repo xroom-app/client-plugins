758859d044f15d3838dfe28051e429e193054530
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-05-22 18:17:57 +0300

    keep main container full-sized

--- nisdos/container/src/iframePlaceholder.js
+++ nisdos/container/src/iframePlaceholder.js
@@ -1,6 +1,6 @@
 export default `
-<html>
-<body style="margin: 0; padding: 0;display: flex;justify-content: center;align-items: center">
+<html style="height: 100%">
+<body style="margin: 0; padding: 0;display: flex;justify-content: center;align-items: center; height: 100%">
   <div style="font: 300 2.5rem Roboto,sans-serif;color: #444">
     Content will be shown here
   </div>
