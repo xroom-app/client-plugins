7b24bf7fef37ab90f2c4412b589cb71a7a1037f7
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-05-30 15:36:13 +0300

    new icon

--- nisdos/extra-themes/src/index.js
+++ nisdos/extra-themes/src/index.js
@@ -44,9 +44,7 @@ xroom.plugin = {
       onClick: () => this.uiRef.toggleShow(),
       svg: props =>
         <svg width={props.size || 25} height={props.size || 25} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path stroke={props.color} d="M26 5H6a1 1 0 00-1 1v20c0 .6.4 1 1 1h20c.6 0 1-.4 1-1V6c0-.6-.4-1-1-1z" stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round" />
-          <path stroke={props.color} d="M27 20l-5.3-5.3a1 1 0 00-1.4 0l-5.6 5.6a1 1 0 01-1.4 0l-2.6-2.6a1 1 0 00-1.4 0L5 22" stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round" />
-          <path stroke={props.color} d="M12.5 13a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round" />
+          <path stroke={props.color} d="M9 4h17a1 1 0 011 1v13a1 1 0 01-1 1H6a1 1 0 01-1-1V8a4 4 0 014-4v0zM14 19l-1 7a3 3 0 006 0l-1-7M5 14h22M21 4v5" stroke-width={1.5 * 32/25} stroke-linecap="round" stroke-linejoin="round" />
         </svg>
     })
   }
