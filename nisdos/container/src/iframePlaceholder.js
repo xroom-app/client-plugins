61b06b07da3a76708172868ad85c49460a4b0547
Vladimir Osipov <vladimir.osipov@coursio.com>
2021-06-12 23:16:36 +0300

    center text just in case (for smaller screens)

--- nisdos/container/src/iframePlaceholder.js
+++ nisdos/container/src/iframePlaceholder.js
@@ -1,7 +1,7 @@
 export default `
 <html style="height: 100%">
 <body style="margin: 0; padding: 0;display: flex;justify-content: center;align-items: center; height: 100%">
-  <div style="font: 300 2.5rem Roboto,sans-serif;color: #444">
+  <div style="font: 300 2.5rem Roboto,sans-serif;color: #444;text-align: center">
     Content will be shown here
   </div>
 </body>
